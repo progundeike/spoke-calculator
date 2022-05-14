a0c56455675634eb83eccc5b920907eaca3d3e36
progundeike <fundeike@yahoo.co.jp>
2022-05-15 08:08:43 +0900

    debugを修正

--- app/Http/Controllers/CalcTensionDiffController.php
+++ app/Http/Controllers/CalcTensionDiffController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\Log;
 use Illuminate\Http\Request;
 use App\Http\Requests\SpokeLengthListRequest;
 use Session;
@@ -19,14 +18,9 @@ class CalcTensionDiffController extends Controller
         //垂直方向の長さ
         $Yr = ($session['erd'] / 2) - ($session['pcdR'] / 2) * cos($crossRadR);
         $Yl = ($session['erd'] / 2) - ($session['pcdL'] / 2) * cos($crossRadL);
-        Log::debug($Yr);
-        Log::debug($Yl);
-
         //水平方向の長さ
         $Xr = $session['centerFlangeR'] - ($session['nippleHoleGap'] / 2) + $session['rimOffset'];
         $Xl = $session['centerFlangeL'] - ($session['nippleHoleGap'] / 2) - $session['rimOffset'];
-        Log::debug($Xr);
-        Log::debug($Xl);
         //スポークテンションの左右差(L/R)
         $LRratio = (float) $Xr / $Xl * sqrt(($Xl ** 2) + ($Yl ** 2)) / sqrt(($Xr ** 2) + ($Yr ** 2)) * 100;
         $LRratio = round($LRratio, 2);
